ef5478d767e464d516ae790530603ff3d42185e0
Wicaebeth <theo.meneboode.private@gmail.com>
2025-02-26 11:45:36 +0100

    Modifications de nom

--- tests/tests_temp.py
+++ tests/tests_temp.py
@@ -1,11 +1,14 @@
-from src.Temp import Temperature
+from src.temperature import Temperature
+
 
 def test_celsius_to_fahrenheit():
     temp = Temperature()
     assert temp.celsius_to_fahrenheit(0) == 32
     assert temp.celsius_to_fahrenheit(100) == 212
 
+
 def test_fahrenheit_to_celsius():
     temp = Temperature()
     assert temp.fahrenheit_to_celsius(32) == 0
     assert temp.fahrenheit_to_celsius(212) == 100
+
