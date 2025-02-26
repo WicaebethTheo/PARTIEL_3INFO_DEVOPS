2a103c8a0ad73fef7088cd408c70e3111690f30a
Wicaebeth <theo.meneboode.private@gmail.com>
2025-02-26 11:42:30 +0100

    Add temperature converter with tests

---
+++ src/Temp.py
@@ -0,0 +1,7 @@
+class Temperature:
+
+    def celsius_to_fahrenheit(self, celsius):
+        return (celsius * 9/5) + 32
+
+    def fahrenheit_to_celsius(self, fahrenheit):
+        return (fahrenheit - 32) * 5/9
---
+++ tests/tests_temp.py
@@ -0,0 +1,11 @@
+from src.Temp import Temperature
+
+def test_celsius_to_fahrenheit():
+    temp = Temperature()
+    assert temp.celsius_to_fahrenheit(0) == 32
+    assert temp.celsius_to_fahrenheit(100) == 212
+
+def test_fahrenheit_to_celsius():
+    temp = Temperature()
+    assert temp.fahrenheit_to_celsius(32) == 0
+    assert temp.fahrenheit_to_celsius(212) == 100
